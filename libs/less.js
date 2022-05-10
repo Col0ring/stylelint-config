9b02f8a96c9d324a36f96d4a2c7c2b9589a36f4d
Col0ring <1561999073@qq.com>
2022-05-11 02:19:43 +0800

    fix: use postcss-less

--- libs/less.js
+++ libs/less.js
@@ -3,11 +3,8 @@ module.exports = {
   overrides: [
     {
       files: ['**/*.less'],
-      // postcss-less 会引起报错，暂时不用这个，直接关 at-rule-no-unknown
-      // customSyntax: 'postcss-less',
+      customSyntax: 'postcss-less',
       rules: {
-        'no-invalid-double-slash-comments': null,
-        'at-rule-no-unknown': null,
         'selector-pseudo-class-no-unknown': [
           true,
           {
