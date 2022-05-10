4fdd361e9fc2ecac2831656b77e4493dc208b63e
Col0ring <1561999073@qq.com>
2022-05-11 02:00:49 +0800

    fix: less comments

--- libs/less.js
+++ libs/less.js
@@ -6,6 +6,7 @@ module.exports = {
       // postcss-less 会引起报错，暂时不用这个，直接关 at-rule-no-unknown
       // customSyntax: 'postcss-less',
       rules: {
+        'no-invalid-double-slash-comments': null,
         'at-rule-no-unknown': null,
         'selector-pseudo-class-no-unknown': [
           true,
